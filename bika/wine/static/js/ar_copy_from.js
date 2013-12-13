817aa7bae94b68e069c2bbb73cfffafd57d60a59
Campbell <campbell@bikalabs.com>
2013-12-13 17:24:14 +0200

    Copy Analysis specs when copying an AR

--- bika/wine/static/js/ar_copy_from.js
+++ bika/wine/static/js/ar_copy_from.js
@@ -1,57 +1,79 @@
+/*global jQuery:false */
 (function( $ ) {
-$(document).ready(function(){
+"use strict";
 
-	_p = jarn.i18n.MessageFactory('plone');
-	_b = jarn.i18n.MessageFactory('bika');
-	_ = jarn.i18n.MessageFactory('bika.wine');
+function fill_column(data) {
+	if(data.objects.length > 0) {
+		var obj = data.objects[0];
+		var col = window.bika.ar_copy_from_col;
+		$("#ar_"+col+"_Contact").val(obj.Contact);
+		$("#ar_"+col+"_Contact_uid").val(obj.Contact_uid);
+		// $("#ar_"+col+"_CCContact").val(obj.CCContact);
+		// $("#ar_"+col+"_CCContact_uid").val(obj.CCContact_uid);
+		$("#ar_"+col+"_CCEmails").val(obj.CCEmails);
+		$("#ar_"+col+"_Client").val(obj.Client);
+		$("#ar_"+col+"_Client_uid").val(obj.Client_uid);
+		$("#ar_"+col+"_SubGroup").val(obj.SubGroup);
+		$("#ar_"+col+"_SubGroup_uid").val(obj.SubGroup_uid);
+		$("#ar_"+col+"_Template").val(obj.Template);
+		$("#ar_"+col+"_Template_uid").val(obj.Template_uid);
+		$("#ar_"+col+"_Profile").val(obj.Profile);
+		$("#ar_"+col+"_Profile_uid").val(obj.Profile_uid);
+		$("#ar_"+col+"_SampleType").val(obj.SampleType);
+		$("#ar_"+col+"_SampleType_uid").val(obj.SampleType_uid);
+		$("#ar_"+col+"_SamplePoint").val(obj.SamplePoint);
+		$("#ar_"+col+"_SamplePoint_uid").val(obj.SamplePoint_uid);
+		$("#ar_"+col+"_ClientOrderNumber").val(obj.ClientOrderNumber);
+		$("#ar_"+col+"_ClientReference").val(obj.ClientReference);
+		$("#ar_"+col+"_ClientSampleID").val(obj.ClientSampleID);
 
-	var copy_from = window.location.href.split("copy_from=");
-	var arinfourl = window.location.href.split("/portal_factory")[0] + "/getarinfo"
-	if(copy_from.length > 1){
-		copy_from = copy_from[1].split("&")[0]
-		copy_from = copy_from.split(",")
-		for (var col = 0; col < copy_from.length; col++) {
-			jQuery.ajaxSetup({async:false});
-			$.getJSON(arinfourl,
-				{'ar_uid': copy_from[col],
-				 'column': col,
-				 '_authenticator': $('input[name="_authenticator"]').val(),},
-				function(ar_data, textStatus){
-					column = ar_data['column'];
-					$("#ar_"+column+"_Contact").val(ar_data['Contact']);
-					$("#ar_"+column+"_Contact_uid").val(ar_data['Contact_uid']);
-					// $("#ar_"+column+"_CCContact").val(ar_data['CCContact']);
-					// $("#ar_"+column+"_CCContact_uid").val(ar_data['CCContact_uid']);
-					$("#ar_"+column+"_CCEmails").val(ar_data['CCEmails']);
-					$("#ar_"+column+"_Client").val(ar_data['Client']);
-					$("#ar_"+column+"_Client_uid").val(ar_data['Client_uid']);
-					$("#ar_"+column+"_SubGroup").val(ar_data['SubGroup']);
-					$("#ar_"+column+"_SubGroup_uid").val(ar_data['SubGroup_uid']);
-					$("#ar_"+column+"_Template").val(ar_data['Template']);
-					$("#ar_"+column+"_Template_uid").val(ar_data['Template_uid']);
-					$("#ar_"+column+"_Profile").val(ar_data['Profile']);
-					$("#ar_"+column+"_Profile_uid").val(ar_data['Profile_uid']);
-					$("#ar_"+column+"_SampleType").val(ar_data['SampleType']);
-					$("#ar_"+column+"_SampleType_uid").val(ar_data['SampleType_uid']);
-					$("#ar_"+column+"_SamplePoint").val(ar_data['SamplePoint']);
-					$("#ar_"+column+"_SamplePoint_uid").val(ar_data['SamplePoint_uid']);
-					$("#ar_"+column+"_ClientOrderNumber").val(ar_data['ClientOrderNumber']);
-					$("#ar_"+column+"_ClientReference").val(ar_data['ClientReference']);
-					$("#ar_"+column+"_ClientSampleID").val(ar_data['ClientSampleID']);
+		var services = {};
+		var specs = {};
+		var cat_uid, service_uid;
 
-					for (var i = ar_data['categories'].length - 1; i >= 0; i--) {
-						var cat_uid = ar_data['categories'][i];
-						var services = ar_data['services'][cat_uid];
-						window.toggleCat('lab', cat_uid, column, [], true);
-						for (var s = 0; s < services.length; s++) {
-							$("[column="+column+"]").filter("#"+services[s]).attr('checked', 'checked');
-						};
-					};
-					window.calculate_parts(column);
+		for (var i = obj.Analyses.length - 1; i >= 0; i--) {
+			var analysis = obj.Analyses[i];
+			cat_uid = analysis.CategoryUID;
+			service_uid = analysis.ServiceUID;
+			if(!(analysis.CategoryUID in services)){
+				services[analysis.CategoryUID] = [];
+			}
+			services[analysis.CategoryUID].push(service_uid);
+			specs[service_uid] = analysis.specification;
+		}
+
+		for(cat_uid in services){
+			if(!services.hasOwnProperty(cat_uid)){ continue; }
+			var service_uids = services[cat_uid];
+			window.toggleCat("lab", cat_uid, col, [], true);
+			for (cat_uid = 0; cat_uid < service_uids.length; cat_uid++) {
+				service_uid = service_uids[cat_uid];
+				$("[column="+col+"]").filter("#"+service_uid).click();
+				var spec = specs[service_uid];
+				if(spec){
+					$("[name^='ar."+col+".min']").filter("[uid='"+service_uid+"']").val(spec.min);
+					$("[name^='ar."+col+".max']").filter("[uid='"+service_uid+"']").val(spec.max);
+					$("[name^='ar."+col+".error']").filter("[uid='"+service_uid+"']").val(spec.error);
 				}
-			);
-			jQuery.ajaxSetup({async:true});
-		};
+			}
+		}
+	}
+}
+
+$(document).ready(function(){
+	var copy_from = window.location.href.split("copy_from=");
+	if(copy_from.length > 1){
+		copy_from = copy_from[1].split("&")[0];
+		copy_from = copy_from.split(",");
+		for (var column = 0; column < copy_from.length; column++) {
+			window.bika.ar_copy_from_col = column;
+			$.ajaxSetup({async:false});
+			window.bika.lims.jsonapi_read({
+				catalog_name: "uid_catalog",
+				UID: copy_from[column]
+			}, fill_column);
+			$.ajaxSetup({async:true});
+		}
 	}
 
 });
